0330e07b179d78d806bf0c9b64e58dc92044ebf5
Ljm <2262754355@qq.com>
2023-05-08 21:41:51 +0800

    feat: 自定义组件

---
+++ components/ABox.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="a-box">
+    <slot >
+    <h3 >hellow word</h3>
+  </slot>
+  </div>
+ 
+  
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+h3{
+  color: red;
+}
+@keyframes ss {
+  from{
+    opacity: 0;
+    transform: translateY(10%);
+  }to{
+    opacity: 1;
+  }
+}
+.a-box{
+  animation: ss linear  .5s ;
+}
+</style>
---
+++ pages/posts/test.md
@@ -0,0 +1,12 @@
+---
+title: 测试
+date: 2023-5-7
+updated: 2023-5-7
+excerpt_type: html
+top: 1
+---
+### test
+
+<a-box></a-box>
+
+### title2
