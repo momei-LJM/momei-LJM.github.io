00d57e34b318579bdf08555b6de990850d5cf736
Ljm <2262754355@qq.com>
2023-05-15 23:34:28 +0800

    feat: change

--- components/IFrame.vue
+++ components/IFrame.vue
@@ -1,23 +1,29 @@
 <template>
   <div>
-<iframe class="if" :src="src"></iframe>
-<!-- <iframe src="/aa.html"></iframe> -->
+<iframe :style="style" :src="src"></iframe>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent ,computed} from 'vue'
 
 export default defineComponent({
   props:{
     src:{
       type:String
+    },
+    width:{
+      type:String,
+      default:'100%'
+    },
+    height:{
+      type:String,
+      default:'500px'
     }
   },
-  setup () {
-    
-
-    return {}
+  setup (props) {
+    const style = computed(()=>({width:props.width,height:props.height}))
+    return {style}
   }
 })
 </script>
