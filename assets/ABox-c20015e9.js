3f6b0c399cc381f96246afeeb242b1d610b67939
momei-LJM <78132554+momei-LJM@users.noreply.github.com>
2023-12-22 08:04:43 +0000

    Deploying to gh-pages from @ momei-LJM/momei-LJM.github.io@8b97c532e9ae7136d3e8c9ec924c59708679469e 🚀

--- assets/ABox-c20015e9.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as s,g as _,r as c,h as t,i as a,a as n}from"./app-065ff51b.js";const d={},r=e=>(t("data-v-36a9c760"),e=e(),a(),e),p={class:"a-box"},l=r(()=>n("h3",null,"hellow word",-1));function i(e,u){return s(),_("div",p,[c(e.$slots,"default",{},()=>[l],!0)])}const f=o(d,[["render",i],["__scopeId","data-v-36a9c760"]]);export{f as _};
