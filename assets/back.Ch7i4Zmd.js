680ebd0870ab7e356578d1b2bb4e049f69b72ee4
momei-LJM <78132554+momei-LJM@users.noreply.github.com>
2025-03-16 05:33:33 +0000

    Deploying to gh-pages from @ momei-LJM/momei-LJM.github.io@707f2593e289a171cb9a17063b8ddd1beca12a6e 🚀

--- assets/back.Ch7i4Zmd.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./chunks/vue-router.Bn22J_ia.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
