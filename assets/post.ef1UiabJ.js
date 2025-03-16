1e8ecdb2e50000eff2056ae252e9f54015d848c8
momei-LJM <78132554+momei-LJM@users.noreply.github.com>
2025-03-16 13:39:30 +0000

    Deploying to gh-pages from @ momei-LJM/momei-LJM.github.io@80eb37a5b7c13aded9a5965a109767c70bb7685e 🚀

--- assets/post.ef1UiabJ.js
+++
@@ -1,1 +0,0 @@
-import{o as e}from"./app.BGYTvaiO.js";import{c as i}from"./framework.DntyxCiH.js";function l(o){if(!o)return{color:"",icon:"",styles:void 0};const r=e();o in r.value.types||(o="link");const s=r.value.types[o].color,n=r.value.types[o].icon,c=i(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:n,styles:c}}export{l as u};
