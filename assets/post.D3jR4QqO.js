bf28f774b3fee739def4982b2130d27ec460f66b
momei-LJM <2262754355@qq.com>
2025-09-29 02:04:24 +0000

    Deploying to gh-pages from @ momei-LJM/momei-LJM.github.io@5287e9ad188b3e72f5981c7f5b94bf4d7acd29b8 🚀

---
+++ assets/post.D3jR4QqO.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.LTfPClIo.js";import{p as t}from"./app.GT5TpFtn.js";import{M as e}from"./framework.CsAdMATk.js";function m(o){if(!o)return{color:"",icon:"",styles:void 0};const r=t();o in r.value.types||(o="link");const s=r.value.types[o].color,i=r.value.types[o].icon,n=e(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:i,styles:n}}export{m as u};
